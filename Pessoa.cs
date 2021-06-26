9e8774cf74602eb23ea24f73b440e3ba7656dcdd
alexandreBC-creator <alexandrebc666@outlook.com>
2021-06-26 16:13:30 -0300

    SQL_PESSOA

--- Pessoa.cs
+++ Pessoa.cs
@@ -9,8 +9,10 @@ using MySql.Data.MySqlClient;
 using MySql.Data;
 using MySql.Data.MySqlClient;
 
-namespace ControleBiblioteca {
-    class Pessoa {
+namespace ControleBiblioteca
+{
+    class Pessoa
+    {
         public int idpessoa { get; set; }
         public string nome { get; set; }
         public string cpf { get; set; }
@@ -23,12 +25,16 @@ namespace ControleBiblioteca {
         public string bairro { get; set; }
         public string estado { get; set; }
         public string cidade { get; set; }
+        public string cep { get; set; }
+        public int idcontato { get; set; }
 
-        public Pessoa() {
+        public Pessoa()
+        {
 
         }
 
-        public Pessoa(string nome, string cpf, DateTime dataNasc, string telefone, string email, string logradouro, string numero, string bairro, string estado, string cidade) {
+        public Pessoa(string nome, string cpf, DateTime dataNasc, string telefone, string email, string logradouro, string numero, string bairro, string estado, string cidade,string cep)
+        {
             this.nome = nome;
             this.cpf = cpf;
             this.dataNasc = dataNasc;
@@ -39,10 +45,11 @@ namespace ControleBiblioteca {
             this.bairro = bairro;
             this.estado = estado;
             this.cidade = cidade;
-
+            this.cep = cep;
         }
 
-        public Pessoa(string nome, string cpf, DateTime dataNasc, string telefone, string email, string logradouro, string numero, string bairro, string estado, string cidade, int idpessoa, int idendereco) {
+        public Pessoa(string nome, string cpf, DateTime dataNasc, string telefone, string email, string logradouro, string numero, string bairro, string estado, string cidade, int idpessoa, int idendereco)
+        {
             this.nome = nome;
             this.cpf = cpf;
             this.dataNasc = dataNasc;
@@ -58,113 +65,227 @@ namespace ControleBiblioteca {
 
         }
 
-        public string insert() {
+        public string insert()
+        {
             Conexao c = new Conexao();
-            try {
+            try
+            {
                 c.abreConexao();
 
                 MySqlCommand cmd = new MySqlCommand(
-                    @"", c.conexaoBD());
+                    "INSERT INTO ControleBiblioteca.cad_pessoas"
+                    + "(nome, cpf, datanasc) "
+                    + "VALUES(@nome,@cpf, @dataNasc) "
+                , c.conexaoBD());
 
                 //Dados gerais da pessoa
                 cmd.Parameters.AddWithValue("@nome", this.nome);
                 cmd.Parameters.AddWithValue("@cpf", this.cpf);
                 cmd.Parameters.AddWithValue("@dataNasc", this.dataNasc);
+
+                cmd.ExecuteNonQuery();
+
+                
+;
+                cmd = new MySqlCommand("SELECT LAST_INSERT_ID() as ultimo ", c.conexaoBD());
+                MySqlDataReader dados = cmd.ExecuteReader();
+                
+                int last_id = 0;
+
+                if (dados.HasRows)
+                {
+                    last_id = dados.GetInt32(dados.GetOrdinal("ultimo"));
+                }
+                else
+                {
+                    return "Falha na obtenção do ultimo ID";
+                }
+
+                cmd = new MySqlCommand("INSERT INTO ControleBiblioteca.cad_contatos "
+                + "(telefone, email, idpessoa, principal) "
+                + "VALUES(@telefone, @email, @idpessoa, true)", c.conexaoBD());
                 cmd.Parameters.AddWithValue("@telefone", this.telefone);
                 cmd.Parameters.AddWithValue("@email", this.email);
+                cmd.Parameters.AddWithValue("@idpessoa", last_id);
+
+                cmd.ExecuteNonQuery();
+
+                cmd = new MySqlCommand("SELECT LAST_INSERT_ID() as ultimo ", c.conexaoBD());
+                dados = cmd.ExecuteReader();
+
+                int last_id_contact = 0;
+
+                if (dados.HasRows)
+                {
+                    last_id_contact = dados.GetInt32(dados.GetOrdinal("ultimo"));
+                }
+                else
+                {
+                    return "Falha na obtenção do ultimo ID do Contato";
+                }
+
+                cmd = new MySqlCommand("UPDATE ControleBiblioteca.cad_pessoas SET idcontato = @idcontato, WHERE idpessoa= @idpessoa", c.conexaoBD());
+
+                cmd.Parameters.AddWithValue("@idcontato", last_id_contact);
+                cmd.Parameters.AddWithValue("@idpessoa", last_id);
+
+
+                cmd = new MySqlCommand("INSERT INTO ControleBiblioteca.cad_enderecos"
+                + "(logradouro, bairro, numero, estado, cidade, cep, idpessoa)"
+                + "VALUES(@logradouro,@bairro,@numero, @estado, @cidade, @cep, @idpessoa)", c.conexaoBD());
+
+                //Endereço da pessoa
+                cmd.Parameters.AddWithValue("@logradouro", this.logradouro);
+                cmd.Parameters.AddWithValue("@numero", this.numero);
+                cmd.Parameters.AddWithValue("@bairro", this.bairro);
+                cmd.Parameters.AddWithValue("@estado", this.estado);
+                cmd.Parameters.AddWithValue("@cep", this.cep);
+                cmd.Parameters.AddWithValue("@cidade", this.cidade);
+                cmd.Parameters.AddWithValue("@idpessoa", last_id);
+
+                cmd = new MySqlCommand("SELECT LAST_INSERT_ID() as ultimo ", c.conexaoBD());
+                dados = cmd.ExecuteReader();
+
+                int last_id_address = 0;
+
+                if (dados.HasRows)
+                {
+                     = dados.GetInt32(dados.GetOrdinal("ultimo"));
+                }
+                else
+                {
+                    return "Falha na obtenção do ultimo ID do Endereço";
+                }
+
+                cmd = new MySqlCommand("UPDATE ControleBiblioteca.cad_pessoas SET idendereco = @idendereco, WHERE idpessoa= @idpessoa", c.conexaoBD());
+
+                cmd.Parameters.AddWithValue("@idendereco", last_id_address);
+                cmd.Parameters.AddWithValue("@idpessoa", last_id);
+
+                return "Ok";
+            }
+            catch (Exception erro)
+            {
+                return erro.Message;
+            }
+            finally
+            {
+                c.fechaConexao();
+            }
+        }
+
+        public DataTable select()
+        {
+            DataTable tabela = new DataTable();
+            Conexao conexao = new Conexao();
+            try
+            {
+                conexao.abreConexao();
+
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM cad_pessoas", conexao.conexaoBD());
+                MySqlDataReader dados = cmd.ExecuteReader();
+                if (dados.HasRows == true)
+                {
+                    tabela.Load(dados);
+                }
+                else
+                {
+                    tabela = new DataTable("Erro");
+                    tabela.Rows.Add("Nenhuma movimentação encontrada");
+                }
+                return tabela;
+            }
+            catch (Exception erro)
+            {
+                tabela = new DataTable("Erro");
+                tabela.Rows.Add("Erro: " + erro.Message);
+                return tabela;
+            }
+            finally
+            {
+                conexao.fechaConexao();
+            }
+        }
+
+
+        public string update()
+        {
+
+            Conexao c = new Conexao();
+            try
+            {
+                c.abreConexao();
+                MySqlCommand cmd = new MySqlCommand("UPDATE ControleBiblioteca.cad_pessoas SET nome=@nome, " +
+                    " cpf=@cpf, datanasc=@dataNasc, WHERE idpessoa= @idpessoa", c.conexaoBD());
+                //Dados gerais da pessoa
+                cmd.Parameters.AddWithValue("@nome", this.nome);
+                cmd.Parameters.AddWithValue("@cpf", this.cpf);
+                cmd.Parameters.AddWithValue("@dataNasc", this.dataNasc);
+                cmd.Parameters.AddWithValue("@idpessoa", this.idpessoa);
+
+                cmd.ExecuteNonQuery();
+
+                cmd = new MySqlCommand("UPDATE ControleBiblioteca.cad_contatos SET telefone=@telefone, email=@email," + " WHERE idcontato=@idcontato"
+                , c.conexaoBD());
+
+
+                cmd.Parameters.AddWithValue("@telefone", this.telefone);
+                cmd.Parameters.AddWithValue("@email", this.email);
+                cmd.Parameters.AddWithValue("@idcontato", this.idcontato);
+
+                cmd.ExecuteNonQuery();
+
+                cmd = new MySqlCommand("UPDATE ControleBiblioteca.cad_enderecos SET logradouro=@logradouro,bairro=@bairro,"
+                    + "numero=@numero, estado=@estado, cidade=@cidade, cep=@cep, WHERE idendereco=@idendereco"
+                , c.conexaoBD());
                 //Endereço da pessoa
                 cmd.Parameters.AddWithValue("@logradouro", this.logradouro);
                 cmd.Parameters.AddWithValue("@numero", this.numero);
                 cmd.Parameters.AddWithValue("@bairro", this.bairro);
                 cmd.Parameters.AddWithValue("@estado", this.estado);
                 cmd.Parameters.AddWithValue("@cidade", this.cidade);
-                cmd.ExecuteNonQuery();
-                return "Ok";
-            }
-            catch (Exception erro) {
-                return erro.Message;
-            }
-            finally {
-                c.fechaConexao();
-            }
-        }
-
-        public DataTable select() {
-            DataTable tabela = new DataTable();
-            Conexao conexao = new Conexao();
-            try {
-                conexao.abreConexao();
-
-                MySqlCommand cmd = new MySqlCommand("SELECT * FROM cad_pessoas", conexao.conexaoBD());
-                MySqlDataReader dados = cmd.ExecuteReader();
-                if (dados.HasRows == true) {
-                    tabela.Load(dados);
-                }
-                else {
-                    tabela = new DataTable("Erro");
-                    tabela.Rows.Add("Nenhuma movimentação encontrada");
-                }
-                return tabela;
-            }
-            catch (Exception erro) {
-                tabela = new DataTable("Erro");
-                tabela.Rows.Add("Erro: " + erro.Message);
-                return tabela;
-            }
-            finally {
-                conexao.fechaConexao();
-            }
-        }
-
-
-        public string update() {
-
-            Conexao c = new Conexao();
-            try {
-                c.abreConexao();
-                MySqlCommand cmd = new MySqlCommand(@"", c.conexaoBD());
-                //Dados gerais da pessoa
-                cmd.Parameters.AddWithValue("@nome", this.nome);
-                cmd.Parameters.AddWithValue("@cpf", this.cpf);
-                cmd.Parameters.AddWithValue("@dataNasc", this.dataNasc);
-                cmd.Parameters.AddWithValue("@telefone", this.telefone);
-                cmd.Parameters.AddWithValue("@email", this.email);
-                cmd.Parameters.AddWithValue("@idpessoa", this.idpessoa);
-                //Endereço da pessoa
-                cmd.Parameters.AddWithValue("@logradouro", this.logradouro);
-                cmd.Parameters.AddWithValue("@numero", this.numero);
-                cmd.Parameters.AddWithValue("@bairro", this.bairro);
-                cmd.Parameters.AddWithValue("@estado", this.estado);
+                cmd.Parameters.AddWithValue("@cep", this.cep);
                 cmd.Parameters.AddWithValue("@idendereco", this.idendereco);
+
                 cmd.ExecuteNonQuery();
+
                 return "Movimentação atualizada com sucesso!";
             }
-            catch (Exception erro) {
+            catch (Exception erro)
+            {
                 return "Erro: " + erro.Message;
             }
-            finally {
+            finally
+            {
                 c.fechaConexao(); ;
             }
 
 
         }
 
-        public string delete(int codigo) {
+        public string inativar(int codigo)
+        {
 
             Conexao c = new Conexao();
-            try {
+            try
+            {
                 c.abreConexao();
-                MySqlCommand cmd = new MySqlCommand(@"", c.conexaoBD());
-                cmd.Parameters.AddWithValue("@idendereco", idendereco);
-                cmd.Parameters.AddWithValue("@idpessoa", idpessoa);
-                cmd.ExecuteNonQuery();
-                return "Movimentação excluida com sucesso!";
+                MySqlCommand cmd = new MySqlCommand("UPDATE ControleBiblioteca.cad_pessoas SET ativo=false "
+                    + " WHERE idpessoa= " + this.idpessoa
+                 , c.conexaoBD()) ;
+
+
+                cmd.ExecuteNonQuery(); 
+                
+                return "Pessoa desativada com sucesso!";
 
             }
-            catch (Exception erro) {
+            catch (Exception erro)
+            {
                 return "Erro: " + erro.Message;
             }
-            finally {
+            finally
+            {
                 c.fechaConexao();
             }
         }
